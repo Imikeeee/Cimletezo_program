a1a717c07937068c2f5316618c499b0a105e1c89
Imikeeee <38005932+Imikeeee@users.noreply.github.com>
2023-06-13 17:19:05 +0200

    Add files via upload
    Készítsen címletező programot, amely a bekért fizetendő és átvett összeg alapján meghatározza a legkevesebb darabszámú címletet, amivel a visszajáró kifizethető. Csak az érvényben lévő címletekkel dolgozzon!
    
    Példa:
    Fizetendő: 7895Ft
    Átvett összeg: 10000Ft
    
    Visszajár:2105Ft
    
    Címletezve:
    0 x 20000
    0 x 10000
    0 x 5000
    1 x 2000
    0 x 1000
    0 x 500
    0 x 200
    1 x 100
    0 x 50
    0 x 20
    0 x 10
    1 x 5
    
    szebb és rövidebb:
    1 x 2000
    1 x 100
    1 x 5

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ervenyesCimletek = { 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5 };
+
+        Console.WriteLine("Fizetendő összeg: ");
+        int fizetendo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Átvett összeg: ");
+        int atvett = int.Parse(Console.ReadLine());
+
+        int visszajaro = atvett - fizetendo;
+
+        Console.WriteLine("Visszajáró összeg: " + visszajaro);
+
+        Console.WriteLine("Címletezve:");
+
+        foreach (int cimlet in ervenyesCimletek)
+        {
+            int darab = visszajaro / cimlet;
+            visszajaro %= cimlet;
+            Console.WriteLine(darab + " x " + cimlet);
+        }
+    }
+}
